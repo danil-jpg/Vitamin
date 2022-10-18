b939b7aa4b94308ae27c8df6c7f7b507d180a4b1
Danil <danil.polovinkin.1994@gmail.com>
2022-10-18 11:14:06 +0300

    added blank of card Page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,13 @@ import burger from './modules/burger.js'
 import { reviews, choose } from './modules/lending.js'
 import quizPage from './modules/quiz.js'
 import catalogue from './modules/catalogue.js'
+import card from './modules/card.js'
 
 window.addEventListener('DOMContentLoaded', () => {
   const cataloguePage = document.querySelector('.cataloguePage')
   const mainPage = document.querySelector('#mainPage')
   const quiz = document.querySelector('#quizPage')
+  const cardPage1 = document.querySelector('.cardPage1')
 
   if (mainPage) {
     choose()
@@ -19,6 +21,9 @@ window.addEventListener('DOMContentLoaded', () => {
   if (cataloguePage) {
     catalogue()
     reviews()
+  }
+  if (cardPage1) {
+    card()
   }
 
   burger()
---
+++ src/scripts/modules/card.js
@@ -0,0 +1,7 @@
+function card () {
+  const defaultLogo = document.querySelector('.header__logo-icon img')
+
+  defaultLogo.setAttribute('src', 'img/cardPage/white-logo.svg')
+}
+
+export default card
