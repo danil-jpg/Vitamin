5a8d5503e7fc22cc7eb8c76fd326376bad749909
Danil <danil.polovinkin.1994@gmail.com>
2022-10-24 13:20:50 +0300

    added some logic

--- src/scripts/modules/profile.js
+++ src/scripts/modules/profile.js
@@ -1,7 +1,20 @@
 function profile () {
+  const titlesList = document.querySelectorAll('.profile__item')
+  const preChosenTab = document.querySelector('.tabs-block_chosen')
+  const tabsList = document.querySelectorAll('.tabs-block')
   const elemToHide = document.querySelectorAll('.profile__list *:not(.profile__title)')
   const elemToShow = document.querySelector('.profile__title')
   const unsubscribeBtn = document.querySelectorAll('.subscriptions__card-btn')
+
+  titlesList.forEach(item => {
+    item.addEventListener('click', (e) => {
+      preChosenTab.classList.remove('tabs-block_chosen')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    })
+  })
 
   if (window.innerWidth <= 1439) {
     elemToHide.forEach(item => {
