b94da88b9f697c2bc5671a1cf316cf4b4186df69
Danil <danil.polovinkin.1994@gmail.com>
2022-10-24 12:01:31 +0300

    completed 375 adaptation ,added some logic

--- src/scripts/modules/profile.js
+++ src/scripts/modules/profile.js
@@ -1,20 +1,32 @@
 function profile () {
   const elemToHide = document.querySelectorAll('.profile__list *:not(.profile__title)')
   const elemToShow = document.querySelector('.profile__title')
-
-  console.log(elemToHide, elemToShow)
+  const unsubscribeBtn = document.querySelectorAll('.subscriptions__card-btn')
 
   if (window.innerWidth <= 1439) {
     elemToHide.forEach(item => {
       item.classList.toggle('active')
     })
-  }
-  elemToShow.addEventListener('click', () => {
-    elemToHide.forEach(item => {
-      item.classList.toggle('active')
+
+    elemToShow.addEventListener('click', () => {
+      elemToHide.forEach(item => {
+        item.classList.toggle('active')
+      })
+      elemToShow.classList.toggle('active1')
     })
-    elemToShow.classList.toggle('active1')
-  })
+  }
+
+  hideCard(1)
+
+  function hideCard (num) {
+    unsubscribeBtn.forEach(item => {
+      item.addEventListener('click', () => {
+        const parentCard = document.querySelector(`.subscriptions__card-wr:nth-of-type(${num})`)
+        console.log(parentCard)
+        parentCard.classList.add('active')
+      })
+    })
+  }
 }
 
 export default profile
