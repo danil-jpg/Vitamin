f0c8a7380e0a36f3fcd7ec3c95a2251a5fb3358c
Danil <danil.polovinkin.1994@gmail.com>
2022-10-24 13:31:07 +0300

    fixed some logic

--- src/scripts/modules/profile.js
+++ src/scripts/modules/profile.js
@@ -1,5 +1,5 @@
 function profile () {
-  const titlesList = document.querySelectorAll('.profile__item')
+  const titlesList = document.querySelectorAll('.profile__item a')
   const preChosenTab = document.querySelector('.tabs-block_chosen')
   const tabsList = document.querySelectorAll('.tabs-block')
   const elemToHide = document.querySelectorAll('.profile__list *:not(.profile__title)')
@@ -9,6 +9,10 @@ function profile () {
   titlesList.forEach(item => {
     item.addEventListener('click', (e) => {
       preChosenTab.classList.remove('tabs-block_chosen')
+      titlesList.forEach(item => {
+        item.classList.remove('profile__title')
+      })
+      e.target.classList.add('profile__title')
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
