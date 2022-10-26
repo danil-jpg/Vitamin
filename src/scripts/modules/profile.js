e79f39304f5e0eb4b30fc1b9ce02ae1be9cf0328
Danil <danil.polovinkin.1994@gmail.com>
2022-10-26 10:26:33 +0300

    fixed script

--- src/scripts/modules/profile.js
+++ src/scripts/modules/profile.js
@@ -1,17 +1,13 @@
 function profile () {
   const titlesList = document.querySelectorAll('.profile__item a')
   const preChosenTab = document.querySelector('.tabs-block_chosen')
-  const tabsList = document.querySelectorAll('.tabs-block')
   const elemToHide = document.querySelectorAll('.profile__list *:not(.profile__title)')
   const elemToShow = document.querySelector('.profile__title')
   const unsubscribeBtn = document.querySelectorAll('.subscriptions__card-btn')
   const profileCheckoutBtn = document.querySelector('.checkout-form__profile-btn')
   const popUp = document.querySelector('.pop-up')
   const itemTogglers = document.querySelectorAll('.orders__block-top-text-wr')
-  const cardToHide = document.querySelectorAll('.orders__block-wr > *:not(.orders__block-top-text-wr)')
   const orderArrow = document.querySelectorAll('.orders__bottom-text')
-
-  console.log(orderArrow)
 
   titlesList.forEach(item => {
     item.addEventListener('click', (e) => {
@@ -38,20 +34,19 @@ function profile () {
         item.classList.toggle('active')
       })
     })
-    itemTogglers[0].addEventListener('click', () => {
-      const elemsToHide2 = document.querySelectorAll(`.orders__block-wr:nth-of-type(${0}) > *:not(.orders__block-top-text-wr)`)
-      console.log('clicked')
-      elemsToHide2.forEach(item => {
-        item.classList.toggle('order-collapse')
-        orderArrow[0].classList.toggle('order-rotate-arrow')
-      })
-    })
+
     collapseCard(0)
     collapseCard(1)
   }
 
   function collapseCard (i) {
-
+    itemTogglers[i].addEventListener('click', () => {
+      const elemsToHide2 = document.querySelectorAll(`.orders__block-wr:nth-of-type(${i + 1}) > *:not(.orders__block-top-text-wr)`)
+      elemsToHide2.forEach(item => {
+        item.classList.toggle('order-collapse')
+        orderArrow[i].classList.toggle('order-rotate-arrow')
+      })
+    })
   }
 
   for (let i = 1; i <= 3; i++) {
